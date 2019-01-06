52103b9eca8f61910a3c31bbe690b14116d6c79e
Christian Henry <christian.henry92@gmail.com>
2019-01-06 14:58:27 -0500

    Add deploy config

---
+++ nix/service-configuration.nix
@@ -0,0 +1,24 @@
+{
+  network.description = "Happy hour servers";
+
+  backend1 =
+    { config, pkgs, ... }: 
+    let
+      backend = (import ./backend.nix {}).backend;
+    in
+    { 
+      networking.hostName = "backend1";
+
+      networking.firewall.allowedTCPPorts = [ 22 80 3000 ];
+      environment.systemPackages = [ backend ];
+
+      systemd.services.backend =
+        { description = "Happy Hour Webserver";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+          serviceConfig =
+            { ExecStart = "${backend}/bin/backend";
+            };
+        };
+    };
+}
