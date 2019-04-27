1300626ef0c0c389df8215582f6d6953eaec8d43
Christian Henry <christian.henry92@gmail.com>
2019-04-27 12:07:39 -0400

    Kill unused autocomplete code

--- frontend/src/Autocomplete.hs
+++ frontend/src/Autocomplete.hs
@@ -19,36 +19,11 @@ import Control.Concurrent.MVar (takeMVar, putMVar, newEmptyMVar)
 import Data.String (fromString)
 import Data.Text
 import Language.Javascript.JSaddle
-import Language.Javascript.JSaddle.Classes
-import Language.Javascript.JSaddle.Object
-import Language.Javascript.JSaddle.Value
 import GHCJS.DOM.Element (Element)
 import Reflex.Dom.Core hiding (Element)
 
 import Common.Dto
 import FrontendCommon
-
--- autocompleteBox :: MonadWidget t m =>
---   m ()
--- autocompleteBox = do 
---   inputElement <- _textInput_element <$> textInput def
---   autocomplete <- liftJSM $ new (jsg "Autocomplete") [inputElement]
---   return ()
-
--- autocompleteBoxMain :: MonadWidget t m => m ()
--- autocompleteBoxMain = do 
---   eBtnClicked <- button (fromString "Attach")
---   (e, _) <- elAttr' (fromString "input") ((fromString "type") =: (fromString "text") <> (fromString "id") =: (fromString "myLabel")) $ blank 
---   performEvent $ liftJSM (autocompleteBox $ _element_raw e) <$ eBtnClicked
---   -- widgetHold blank (mkAutocompleteBox <$> eBtnClicked)
---   -- mkAutocompleteBox <$> eBtnClicked
---   return ()
-
--- autocompleteBox :: Element -> JSM ()
--- autocompleteBox parent = void $ do 
---     autocompleteBox <- printJSE $ new (jsg "google.maps.places.Autocomplete") ()
---     text <- valToText autocompleteBox
---     liftIO $ putStrLn (show text)
 
 eAutocompleteBox :: MonadWidget t m
   => Element
@@ -60,12 +35,8 @@ eAutocompleteBox parent = do
 
 autocompleteBox :: Element -> (Maybe (Text, LL, Text) -> JSM ()) -> JSM ()
 autocompleteBox parent = \applyResults -> void $ do
-  -- options <- new (jsg "google" ^. js "maps" . js "places" . js "AutocompleteOptions") ()
-  -- options 
   autocompleteObj <- new (jsg "google" ^. js "maps" . js "places" . js "Autocomplete") (toJSVal parent)
   autocompleteObj ^. jsf "setTypes" [["establishment"]]
-  -- bounds <- makeLatLngBounds
-  -- autocompleteObj ^. jsf "setBounds" [bounds]
   autocompleteObj ^. jsf "addListener" ("place_changed", fun $ \_ _ _ -> void $ do       
         placeResult <- autocompleteObj ^. js0 "getPlace"
 
@@ -82,13 +53,6 @@ autocompleteBox parent = \applyResults -> void $ do
         -- TODO: get city from address_components: long_name when types contains "locality"
         applyResults $ (,,) <$> placeId <*> latlng <*> pure (fromString "Detroit")
     )
-
--- coerceTuple :: JSVal -> JSVal -> JSVal -> Maybe (a, b, c)
--- coerceTuple a b c = do 
---   a' <- fromJSVal a
---   b' <- fromJSVal b 
---   c' <- fromJSVal c
---   return (a', b', c')
 
 makeLatLngBounds :: JSM Object
 makeLatLngBounds = do 
