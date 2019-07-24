46b4a681b3855d0d69a62f40478e3af2da36c25e
Christian Henry <christian.henry92@gmail.com>
2019-07-24 10:40:22 -0400

    Update gitignore, shell for hie code

--- nix/backend/shell.nix
+++ nix/backend/shell.nix
@@ -3,13 +3,15 @@
 let 
   pinnedPkgs = import ../pkgs-from-json.nix { json = ../nixos-18-09.json; };
   myPackages = (import ./backend.nix { withHoogle = false; } );
-
+  # all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
   projectDrvEnv = myPackages.backend.env.overrideAttrs (oldAttrs: rec {
     buildInputs = oldAttrs.buildInputs ++ [ 
       pinnedPkgs.haskellPackages.hlint
       pinnedPkgs.haskellPackages.cabal-install
       pinnedPkgs.haskellPackages.hsimport
+      # all-hies.versions.ghc843
       ];
+    # export HIE_HOOGLE_DATABASE="$NIX_GHC_LIBDIR/../../share/doc/hoogle/index.html"
     shellHook = ''
       export ES_PASSWORD=${builtins.readFile ./es_password.secret}
     '';
